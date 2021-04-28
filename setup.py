d80e25cca730eac75016038cf3a9377a7ab35d73
franwe <franziska.wehrmann@gmail.com>
2021-04-28 09:57:05 +0300

    add requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="spd_trading",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/franwe/spd_trading",
-    author="Franziska Wehrmann",
+    project_urls={
+        "Documentation": "https://spd_trading.readthedocs.io/en/latest/",
+        "Code": "https://github.com/franwe/spd_trading",
+    },
+    author="franwe",
     author_email="franziska.wehrmann@gmail.com",
-    description="Description of my package",
+    description="Estimates the Risk Neutral Density and Historical Density of an underlying and suggests trading intervals based on the Pricing Kernel.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
     packages=find_packages(),
-    install_requires=["numpy >= 1.11.1"],
+    python_requires=">=3",
+    install_requires=["numpy", "pandas", "scipy", "arch", "localpoly", "sklearn", "matplotlib", "statsmodels", "scipy"],
 )
